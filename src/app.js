8c5c5e3d2dfd777cc8edf7b7e21a992f47e19cf3
paulowos <paulowos@gmail.com>
2023-07-19 16:27:25 -0300

    Implementação middlewares de segurança

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = require('./routes');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+require('express-async-error');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(rateLimit({
+  max: 100,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from same IP'
+}));
+app.use(morgan('dev'));
 app.use(express.json());
-
 app.use(router);
 
 module.exports = app;
