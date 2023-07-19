5f9b42f643d322b84492c244ba14bb91967cfb3e
paulowos <paulowos@gmail.com>
2023-07-19 12:01:42 -0300

    TDD: Criação do teste para rota GET /people

--- tests/integration/people.test.js
+++ tests/integration/people.test.js
@@ -9,10 +9,26 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testing people endpoint', function () {
-  afterEach(sinon.restore);
+const peopleList = [
+  {
+    id: 1,
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    email: 'luke.skywalker.test.com',
+    phone: '21123456789'
+  },
+  {
+    id: 2,
+    firstName: 'Dart',
+    lastName: 'Vader',
+    email: 'dart.vader.test.com',
+    phone: '11123456789'
+  }
+];
 
-  it('testing person registration', async function () {
+describe('Testing people endpoints', function () {
+  afterEach(sinon.restore);
+  it('testing POST /people', async function () {
     sinon.stub(connection, 'execute').resolves([{ insertId: 42 }]);
 
     const response = await chai.request(app).post('/people').send({
@@ -26,5 +42,14 @@ describe('Testing people endpoint', function () {
     expect(response.body).to.deep.equal({
       message: 'Pessoa cadastrada com sucesso com o id 42',
     });
+  });
+
+  it('testing GET /people', async () => {
+    sinon.stub(connection, 'execute').resolves([peopleList]);
+
+    const response = await chai.request(app).get('/people');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal(peopleList);
   });
 });
